2fd97b18623de4dd3b0819a0979273fb7758e295
Giuseppe Poluboiarinov <giuseppe.po@hotmail.com>
2022-12-23 19:07:53 -0300

    Login Activity finished and application permissions implemented

--- Curso Firestore e Storage/app/src/main/java/com/example/firebasecursosf/LoginActivity.kt
+++ Curso Firestore e Storage/app/src/main/java/com/example/firebasecursosf/LoginActivity.kt
@@ -1,28 +1,30 @@
 package com.example.firebasecursosf
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
-import com.bumptech.glide.util.Util
-import com.example.firebasecursosf.databinding.ActivityAberturaBinding
+import com.br.jafapps.bdfirestore.util.DialogProgress
+import com.example.firebasecursosf.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding: ActivityAberturaBinding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityAberturaBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
+
+        currentUser()
 
         binding.buttonLogin.setOnClickListener(this)
     }
@@ -35,6 +37,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    // Para realizar o login no aplicativo:
     private fun buttonLogin() {
         val email = binding.editEmail.text.toString()
         val password = binding.editPassword.text.toString()
@@ -51,17 +54,40 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
+    // Para receber as informações do firebase:
     private fun handleLogin(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+        // Método para pegar as informações do firebase:
+
+        /*auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
             if (it.isSuccessful) {
                 Toast.makeText(this, "Login efetuado", Toast.LENGTH_SHORT).show()
             } else {
                 val error = it.exception.toString()
                 errorFirebase(error)
             }
+        }*/
+
+        // Outro método para pegar as informações do firebase (melhor que o anterior pq
+        // não precisa fazer o tratamento do sucesso ou falha da resposta do firebase):
+
+        // Para fornecer o sinal de que o aplicativo está carregando ao fazer o login:
+        val dialogProgress = DialogProgress()
+        dialogProgress.show(supportFragmentManager, "1")
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            finish()
+            dialogProgress.dismiss()
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }.addOnFailureListener {
+            dialogProgress.dismiss()
+            val error = it.message.toString()
+            errorFirebase(error)
         }
+
     }
 
+    //Exibição dos erros recebidos do Firebase:
     private fun errorFirebase(error: String) {
         if (error.contains("There is no user record corresponding to this identifier")) {
             Toast.makeText(this, "E-mail não cadastrado", Toast.LENGTH_SHORT).show()
@@ -69,6 +95,16 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             Toast.makeText(this, "Senha inválida", Toast.LENGTH_SHORT).show()
         } else if (error.contains("The email address is badly formatted")) {
             Toast.makeText(this, "Email inválido", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Se o usuário já ter efetuado o login, não precisará realizar novamente, ao menos se deslogar:
+    private fun currentUser() {
+        val user = auth.currentUser
+
+        if (user != null) {
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
--- Curso Firestore e Storage/app/src/main/java/com/example/firebasecursosf/MainActivity.kt
+++ Curso Firestore e Storage/app/src/main/java/com/example/firebasecursosf/MainActivity.kt
@@ -1,12 +1,101 @@
 package com.example.firebasecursosf
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.br.jafapps.bdfirestore.util.Util
+import com.example.firebasecursosf.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardDownloadImage.setOnClickListener(this)
+        binding.cardUploadImage.setOnClickListener(this)
+        binding.cardReadData.setOnClickListener(this)
+        binding.cardSaveChangeRemove.setOnClickListener(this)
+        binding.cardCategories.setOnClickListener(this)
+        binding.cardSignOut.setOnClickListener(this)
+
+        permission()
+        authListener()
+    }
+
+    // Para verificar se as permissões de uso do celular foram fornecidas pelo usuário:
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for (result in grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED) {
+                Util.exibirToast(
+                    this,
+                    "Forneça as permissões para o bom funcionamento do aplicativo"
+                )
+                finish()
+                break
+            }
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            binding.cardDownloadImage.id -> {
+
+            }
+            binding.cardUploadImage.id -> {
+
+            }
+            binding.cardReadData.id -> {
+
+            }
+            binding.cardSaveChangeRemove.id -> {
+
+            }
+            binding.cardCategories.id -> {
+
+            }
+            // Para deslogar do aplicativo:
+            binding.cardSignOut.id -> {
+                finish()
+                Firebase.auth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
+    }
+
+    // Para ouvir quais são os status do usuário, se está logado ou não no Firebase/aplicativo:
+    private fun authListener() {
+        Firebase.auth.addAuthStateListener {
+            if (it.currentUser != null) {
+                Toast.makeText(this, "Login efetuado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Logout efetuado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // Para solicitar as permissões de funcionamento do app ao usuário:
+    private fun permission() {
+        val permissions = arrayOf<String>(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+
+        Util.permissao(this, 100, permissions)
     }
 }
